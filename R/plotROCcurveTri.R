daff184ecb950ca708266f18264e703f1cdceb36
Wu <swu2@fredhutch.org>
2018-09-28 14:14:22 -0700

    Decreased runtime of plotPowerTri and plotROCcurveTri

--- R/plotROCcurveTri.R
+++ R/plotROCcurveTri.R
@@ -127,11 +127,11 @@ computesensspecFPFN <- function(sigma2obs,rho,Plat0,Plat2,P0,P2) {
 #' @examples
 #' Plat0 <- 0.2
 #' Plat2 <- c(0.2, 0.3, 0.4, 0.5)
-#' P0 <- seq(0.90, 0.10, len=25)
-#' P2 <- seq(0.10, 0.90, len=25)
+#' P0 <- seq(0.90, 0.10, len=10)
+#' P2 <- seq(0.10, 0.90, len=10)
 #' rho <- c(1, 0.9, 0.7, 0.5)
 #' plotROCcurveTri(Plat0 = Plat0, Plat2 = Plat2, P0 = P0, P2 = P2, rho = rho)
-#'
+#' 
 #' @export
 plotROCcurveTri <- function(Plat0, Plat2, P0, P2, rho) {
   if(length(rho)!= 4 | any(rho > 1) | any(rho < 0)) {
